c308ff63f625751ab9d0618548aa8f2917950649
Herri2 <pittersonmark75@gmail.com>
2022-11-30 16:28:44 +0200

    Update verifier deploy script

--- scripts/3_deploy_verifier_v3.ts
+++ scripts/3_deploy_verifier_v3.ts
@@ -5,18 +5,29 @@ import hre from "hardhat";
 
 const ISSUER_REGISTRY = "0x8bc1862398D2c03A1dBeE2238E97c8fC9FABB7eC";
 const WHITELIST = "0xF2292e44f294b406484A05942b6717B07a063A23";
+const PROXY_ADMIN_ADDRESS = "0x3958341e490B8a8075F6C84de68563f3586840D9";
+// params 
+
+const PARAM_DEFAULT_AML_GRACETIME_KEY = 3;
+const DEFAULT_GRACETIME_VALUE = 300;
+
+const DEFAULT_AML_RULE = "431050";
+const DEFAULT_KYC_RULE = "777";
+const DEFAULT_KYCAML_RULE = "731090";
+
+const PARAM_TYPE1_DEFAULT_AML_RULE = 4;
+const PARAM_TYPE1_DEFAULT_KYC_RULE = 5;
+const PARAM_TYPE1_DEFAULT_KYCAML_RULE = 6;
 
 
-const RULE = 631050090;
+
 
 async function main(){
     
-    const PROXY_ADMIN = await ethers.getContractFactory("PProxyAdmin");
     const PROXY = await ethers.getContractFactory("PPRoxy");
     const VERIFIER = await ethers.getContractFactory("PureFiVerifier");
 
-    const proxy_admin = await PROXY_ADMIN.deploy();
-    await proxy_admin.deployed();
+    const proxy_admin = await ethers.getContractAt("PProxyAdmin", PROXY_ADMIN_ADDRESS);
 
     console.log("Proxy admin : ", proxy_admin.address);
 
@@ -34,8 +45,15 @@ async function main(){
     await verifier.initialize(ISSUER_REGISTRY, WHITELIST);
 
     // set uint params
-    
 
+    await verifier.setUint256(PARAM_DEFAULT_AML_GRACETIME_KEY, DEFAULT_GRACETIME_VALUE );
+
+    await verifier.setUint256(PARAM_TYPE1_DEFAULT_AML_RULE, DEFAULT_AML_RULE);
+
+    await verifier.setUint256(PARAM_TYPE1_DEFAULT_KYC_RULE, DEFAULT_KYC_RULE);
+
+    await verifier.setUint256(PARAM_TYPE1_DEFAULT_KYCAML_RULE, DEFAULT_KYCAML_RULE);
+     
 }
 
 
